82d229acdaecefe60760455535c84fa236d14407
lorenzo <lorenzocaso335@gmail.com>
2018-06-01 16:56:59 +0200

    fine table

--- ProvaPulley/DrawerContentViewController.swift
+++ ProvaPulley/DrawerContentViewController.swift
@@ -20,6 +20,9 @@ class DrawerContentViewController: UIViewController, UITabBarDelegate, UITableVi
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! PulleyTableViewCell
         
+        let cell1 = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! PulleyTableViewCell1
+        
+        if indexPath.row == 0{
         cell.improf.image = #imageLiteral(resourceName: "Lorenzo")
         cell.sfondo.backgroundColor = .red
         cell.desc.text = "Andiamo a ballare? Waju o drag"
@@ -32,11 +35,34 @@ class DrawerContentViewController: UIViewController, UITabBarDelegate, UITableVi
         cell.num.backgroundColor = .white
         cell.num.textColor = .red
         cell.num.textAlignment = .center
-        
         cell.inizio.text = "22:00"
         cell.inizio.textColor = .white
+        
+        
 
         return cell
+        }else{
+        cell1.improf1.image = #imageLiteral(resourceName: "Giorgio")
+            cell1.inizio1.text = "22:00"
+            cell1.inizio1.textColor = .black
+            cell1.num1.text = "14"
+            cell1.num1.textColor = .white
+            cell1.num1.backgroundColor = .black
+            cell1.num1.textAlignment = .center
+            cell1.num1.layer.cornerRadius = 12.0
+            cell1.num1.clipsToBounds = true
+            cell1.desc1.text = "O DRAGGGGGGGGGGGGGGGGGGGGGGGG"
+            cell1.desc1.textColor = .black
+            cell1.nick1.text = "Giorgia"
+            cell1.nick1.textColor = .black
+            cell1.divisore.text = "-"
+            cell1.divisore.textColor = .black
+            cell1.sfondo1.layer.borderWidth = 1
+            cell1.fine.text = "2:00"
+            cell1.fine.textColor = .black
+        cell1.sfondo1.layer.borderColor = UIColor(red: 0/255, green: 90/255, blue: 50/255, alpha: 1.0).cgColor
+        return cell1
+        }
     }
     
 

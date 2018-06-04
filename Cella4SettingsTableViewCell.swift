cc099d89563f17c6b5f58572cd330f21531c8243
lorenzo <lorenzocaso335@gmail.com>
2018-06-04 12:59:07 +0200

    kjnhijhj

---
+++ Cella4SettingsTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  Cella4SettingsTableViewCell.swift
+//  
+//
+//  Created by Lorenzo Caso on 04/06/18.
+//
+
+import UIKit
+
+class Cella4SettingsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imm: UIImageView!
+    @IBOutlet weak var scritta: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

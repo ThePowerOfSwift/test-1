f0f4b58c50aad73f408a111093df9bf9f7fb06ab
lorenzo <lorenzocaso335@gmail.com>
2018-06-04 12:48:57 +0200

    sett2

---
+++ ProvaPulley/Cella4SettingsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  Cella4SettingsTableViewCell.swift
+//  BatChat
+//
+//  Created by Lorenzo Caso on 04/06/18.
+//  Copyright © 2018 Lorenzo Caso. All rights reserved.
+//
+
+import UIKit
+
+class Cella4SettingsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imm: UIImageView!
+    @IBOutlet weak var scritta: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

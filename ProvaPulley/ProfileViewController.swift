5e99be62e43b418f52cf8f326d8bc59ef2948256
lorenzo <lo.caso@studenti.unina.it>
2018-06-05 15:18:22 +0200

    Ripartire da qua

--- ProvaPulley/ProfileViewController.swift
+++ ProvaPulley/ProfileViewController.swift
@@ -49,9 +49,12 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let vm = UIView()
+        
         vm.backgroundColor = UIColor(red: 229/255.0, green: 229/255.0, blue: 231/255.0, alpha: 1)
         return vm
     }
+    
+    
     
    
     

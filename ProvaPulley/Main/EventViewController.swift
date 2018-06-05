eccc2101217c2ba48099b85a68e20c10554c5ba6
sofiapassarelli <sofiapassarelli@virgilio.it>
2018-06-05 16:58:19 +0200

    eventcontroller

---
+++ ProvaPulley/Main/EventViewController.swift
@@ -0,0 +1,38 @@
+//
+//  EventViewController.swift
+//  BatChat
+//
+//  Created by Sofia Passarelli on 05/06/18.
+//  Copyright © 2018 Lorenzo Caso. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneTapped))
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func doneTapped(){
+        dismissKeyboard()
+        _ = navigationController?.popToRootViewController(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

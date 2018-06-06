6f46307893ef23f1a9b82d7c0fcae6e3b9bd9d3c
lorenzo <lo.caso@studenti.unina.it>
2018-06-06 16:27:58 +0200

    forgot password

---
+++ ProvaPulley/Main/ForgotViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ForgotViewController.swift
+//  BatChat
+//
+//  Created by Lorenzo Caso on 06/06/18.
+//  Copyright © 2018 Lorenzo Caso. All rights reserved.
+//
+
+import UIKit
+
+class ForgotViewController: UIViewController {
+
+    @IBAction func annulla(_ sender: Any) {
+        self.presentingViewController!.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "Login2")!)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

ca17ceaca763e1eade2621a735b79cea28ae7644
Antonio <antonio.terrano8@gmail.com>
2018-06-06 18:54:33 +0200

    aggiunte comunicazioni pulley (forse)

---
+++ ProvaPulley/Main/MessageTableView.swift
@@ -0,0 +1,21 @@
+//
+//  MessageTableView.swift
+//  BatChat
+//
+//  Created by Antonio Terrano on 06/06/18.
+//  Copyright © 2018 Lorenzo Caso. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

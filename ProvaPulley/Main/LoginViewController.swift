34396c84d726ae5af68b85444a0fca7aa42af366
lorenzo <lo.caso@studenti.unina.it>
2018-06-08 15:45:42 +0200

    giorgio2

--- ProvaPulley/Main/LoginViewController.swift
+++ ProvaPulley/Main/LoginViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
     
     @IBOutlet weak var password: UITextField!
@@ -17,35 +17,84 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var logi: UIButton!
     @IBOutlet weak var facebook: UIButton!
     
+    
     @IBAction func Log(_ sender: Any) {
         
-        let result = SingletonServer.singleton.login(email: nick.text!, password: password.text!)
-        
-        //print("ini\(result as! String)fine")
-        
-        print(result)
-        if(result == "Email ok"){
-
-            self.performSegue(withIdentifier: "seguelogin", sender: self)
-
-
-        }else{
-            let alert = UIAlertController(title: "", message: result, preferredStyle: UIAlertControllerStyle.alert)
-
-            alert.addAction(UIAlertAction(title: "ok", style: UIAlertActionStyle.default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
+        SingletonServer.singleton.log(email: nick.text!, password: password.text!) { (result) in
+            
+            let jsonString = result!
+            let data: Data? = jsonString.data(using: .utf8)
+            let json = (try? JSONSerialization.jsonObject(with: data!, options: [])) as? [String:AnyObject]
+            let userRetrieve: DBUser?
+            
+            print(json?["email"])
+            if(json?["email"] != nil){
+                userRetrieve = DBUser(email: json!["email"] as! String, nickname: json!["nickname"] as! String, password: json!["password"] as! String, socialAvatar: json!["socialAvatar"] as! String, token: json!["token"] as! String)
+                print("ATT")
+                DispatchQueue.main.async {
+                    NotificationCenter.default.post(name: NSNotification.Name(rawValue: "perfse"), object: nil)
+                }
+                
+                
+            }else{
+                DispatchQueue.main.async {
+                    let alert = UIAlertController(title: "", message: result, preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "ok", style: UIAlertActionStyle.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+            
         }
+        
+        
     }
     
+//    @IBAction func Log(_ sender: Any) {
+//        let user = DBUser()
+//        user.email = nick.text
+//        user.nickname = " nicj"
+//        user.password = "d dd"
+//        user.token = "ddd"
+//        UserDefaults.standard.set(user, forKey: "nome")
+//
+//
+//        var userDefaults = UserDefaults.standard
+//        let encodedData: Data = NSKeyedArchiver.archivedData(withRootObject: user)
+//        userDefaults.set(encodedData, forKey: "user")
+//        userDefaults.synchronize()
+//
+//
+//        let decoded  = userDefaults.object(forKey: "teams") as! Data
+//        let userdec = NSKeyedUnarchiver.unarchiveObject(with: decoded) as! DBUser
+//
+//
+//       let result = SingletonServer.singleton.login(email: nick.text!, password: password.text!)
+//
+//
+//
+//       print(userdec.email)
+//        if(userdec.email == "giorgio"){
+//
+//            self.performSegue(withIdentifier: "seguelogin", sender: self)
+//
+//        }else{
+//            let alert = UIAlertController(title: "", message: result, preferredStyle: UIAlertControllerStyle.alert)
+//
+//            alert.addAction(UIAlertAction(title: "ok", style: UIAlertActionStyle.default, handler: nil))
+//            self.present(alert, animated: true, completion: nil)
+//        }
+//    }
+    
+ 
     @IBOutlet weak var imm: UIImageView!
     
     @IBOutlet var vista: UIView!
     
     override func viewDidAppear(_ animated: Bool) {
-        let currentUser = DBUser()
-        if currentUser != nil {
-            loadHomeScreen()
-        }
+//        let currentUser = DBUser()
+//        if currentUser.password != nil && currentUser.nickname != nil{
+//            loadHomeScreen()
+//        }
     }
     func loadHomeScreen(){
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
@@ -61,6 +110,8 @@ class LoginViewController: UIViewController {
         facebook.layer.cornerRadius = 13.0
         facebook.clipsToBounds = true
         
+        password.delegate = self as! UITextFieldDelegate
+        nick.delegate = self as! UITextFieldDelegate
         
         register.layer.cornerRadius = 13.0
         register.clipsToBounds = true
@@ -78,7 +129,22 @@ class LoginViewController: UIViewController {
         
         logi.backgroundColor = .orange
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        setupTable()
     }
+    @objc func performa() {
+        self.performSegue(withIdentifier: "seguelogin", sender: self)
+    }
+    
+    func setupTable() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(performa), name: NSNotification.Name(rawValue: "perfse"), object: nil)
+        
+        
+    }
+    
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
--- ProvaPulley/Main/ServerSingleton.swift
+++ ProvaPulley/Main/ServerSingleton.swift
@@ -16,81 +16,36 @@ class SingletonServer{
         
     }
     
-    //    func log(email: String, password: String, completionHandler: @escaping(String?) -> Void) {
-    // invia la richiesta con i dati
-    
-    ////        session.dataTask(with: urlRequest) {
-    ////            data, response, error in
-    //        //          if error!= nil {
-    ////        completionHandler(erro.locali)
-    //    } else {
-    //    completionHandler(data.string)
-    //    }
-    //
-    //
-    ////            guard error == nil else {
-    ////                print(error)
-    ////                complitionHandler(error.localize)
-    ////            }
-    ////            s = String(data: data!, encoding: .utf8)!
-    ////            print(s)
-    ////            }.resume()
-    
-    //    }
-    
-    //    func prova() {
-    //        self.log(email: "ciao", password: "ciaociao") { (data) in
-    //            <#code#>
-    //        }
-    //    }
-    
-    func login(email:String,password:String)->String{
+    func log(email: String, password: String, completionHandler: @escaping(String?) -> Void) {
         let httpMethod = "POST"
         let httpBody : String = password
         
         let textUrl : String = "http://10.20.49.196:8181/login/\(email)/"
         let url : URL = URL(string: textUrl)!
         let session : URLSession = URLSession.shared
-
+        
         var urlRequest : URLRequest = URLRequest(url: url)
         urlRequest.httpBody = httpBody.data(using: .utf8)
         urlRequest.httpMethod = httpMethod
         
-        var s3:String=""
+        
         var s:String?
-        
-        let semaphore = DispatchSemaphore(value: 0)
-        
-        
+        //        let semaphore = DispatchSemaphore(value: 0)
         session.dataTask(with: urlRequest) {
             data, response, error in
-            
-            guard error == nil else {
-                print(error)
-                return
+            if error != nil {
+                print(error?.localizedDescription)
+                completionHandler(error?.localizedDescription)
+            } else {
+                s = String(data: data!, encoding: .utf8)!
+                completionHandler(s)
             }
-            
-            
-            
-            s = String(data: data!, encoding: .utf8)!
-            print(s)
-            
-            semaphore.signal()
-            //                let decoder : JSONDecoder = JSONDecoder()
-            //                do {
-            //                    s1 = try decoder.decode(SiteMessage.self, from: data!).message
-            ////                    print(s1)
-            //                } catch {
-            //                    return
-            //                }
-            //
-            //            }
-            
             }.resume()
-        //            print(s)
-        semaphore.wait()
-        return s!
+        
+        
     }
+    
+    
     func sendMessagePOST(x:Double,y:Double,userEmail:String)->String{
         
         let httpMethod = "POST"

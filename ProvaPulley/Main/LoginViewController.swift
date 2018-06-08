812c421da710142f0d6e66f83bea7e6017d7289b
cescov91 <34346427+cescov91@users.noreply.github.com>
2018-06-08 21:00:55 +0200

    facebook login aggiustato
    facebook login aggiustato anche il bottone

--- ProvaPulley/Main/LoginViewController.swift
+++ ProvaPulley/Main/LoginViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class LoginViewController: UIViewController, UITextFieldDelegate {
+class LoginViewController: UIViewController, UITextFieldDelegate, FBSDKLoginButtonDelegate {
 
     
     @IBOutlet weak var password: UITextField!
@@ -16,6 +18,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var nick: UITextField!
     @IBOutlet weak var logi: UIButton!
     @IBOutlet weak var facebook: UIButton!
+    let loginButton = FBSDKLoginButton()
     
     
     @IBAction func Log(_ sender: Any) {
@@ -125,9 +128,20 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         vista.backgroundColor = .white
         
-        facebook.backgroundColor = UIColor(red: 65/255.0, green: 103/255.0, blue: 178/255.0, alpha: 1)
-        facebook.layer.cornerRadius = 13.0
-        facebook.clipsToBounds = true
+//        facebook.backgroundColor = UIColor(red: 65/255.0, green: 103/255.0, blue: 178/255.0, alpha: 1)
+//        facebook.layer.cornerRadius = 13.0
+//        facebook.clipsToBounds = true
+        
+//        Nuovo tasto di facebook integrato nel pod
+        
+        loginButton.backgroundColor = UIColor(red: 65/255.0, green: 103/255.0, blue: 178/255.0, alpha: 1)
+        loginButton.layer.cornerRadius = 13.0
+        loginButton.clipsToBounds = true
+        loginButton.frame = CGRect(x: 16, y: 514, width: 343, height: 40)
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        
+        loginButton.delegate = self
+        self.view.addSubview(loginButton)
         
         password.delegate = self as! UITextFieldDelegate
         nick.delegate = self as! UITextFieldDelegate
@@ -179,6 +193,42 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+//    funzioni per il login con facebook
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if (error == nil) {
+            print("Utente connesso")
+            print("---------------")
+            print(result.token.userID)
+            print("---------------")
+            print(result.token.permissions)
+            print("---------------")
+            print(result.token.appID)
+            print("---------------")
+            
+        } else
+        {
+            print(error.localizedDescription)
+        }
+        
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, email, age_range, link, gender, locale, timezone, updated_time, verified"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    print(result as Any)
+                }
+            })
+        }
+    }
+    
+    
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Utente disconnesso")
+    }
+    
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        return true
     }
     
 }

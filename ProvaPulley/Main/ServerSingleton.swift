810a1c8cd46f848555d00cbbf5957eef75a0ef66
Giorgio Farina <giorgio.farina@studenti.unina.it>
2018-06-25 01:33:52 +0200

    add answer of an  event

--- ProvaPulley/Main/ServerSingleton.swift
+++ ProvaPulley/Main/ServerSingleton.swift
@@ -260,99 +260,10 @@ class SingletonServer{
         
     }
     
-//    func POST_ADDAnswerQ(text:String,questionID:Int32, email:String, completionHandler: @escaping(String?) -> Void){
-//        let a = DBAnswerQ()
-//        a.text = text
-//        
-//        a.question = DBQuestion()
-//        a.question?.ID = questionID
-//        a.userOwner = DBUser()
-//        a.userOwner?.email = email
-//        let httpMethod = "POST"
-//        let encoder = JSONEncoder()
-//        encoder.outputFormatting = .prettyPrinted
-//        do{
-//            let data = try encoder.encode(a)
-//            let string = String(data:data, encoding: .utf8)
-//            
-//            let httpBody : String = string!
-//            
-//            let textUrl : String = "http://\(ipServer):8181/Question/Answers/add/"
-//            let url : URL = URL(string: textUrl)!
-//            let session : URLSession = URLSession.shared
-//            
-//            var urlRequest : URLRequest = URLRequest(url: url)
-//            urlRequest.httpBody = httpBody.data(using: .utf8)
-//            urlRequest.httpMethod = httpMethod
-//            
-//            
-//            var s:String?
-//            session.dataTask(with: urlRequest) {
-//                data, response, error in
-//                if error != nil {
-//                    print(error?.localizedDescription)
-//                    completionHandler(error?.localizedDescription)
-//                } else {
-//                    s = String(data: data!, encoding: .utf8)!
-//                    completionHandler(s)
-//                }
-//                }.resume()
-//            
-//        }catch{
-//            print("errore di serializzazione")
-//        }
-//        
-//        
-//        
-//        
-//    }
+
     
     
-    func POST_ADDAnswerE(text:String,eventID:Int32, email:String, completionHandler: @escaping(String?) -> Void){
-        let a = DBAnswerE()
-        a.text = text
-        a.event = DBEvent()
-        a.event?.id = eventID
-        a.userOwner = DBUser()
-        a.userOwner?.email = email
-        let httpMethod = "POST"
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = .prettyPrinted
-        do{
-            let data = try encoder.encode(a)
-            let string = String(data:data, encoding: .utf8)
-            
-            let httpBody : String = string!
-            
-            let textUrl : String = "http://\(ipServer):8181/Event/Answers/add/"
-            let url : URL = URL(string: textUrl)!
-            let session : URLSession = URLSession.shared
-            
-            var urlRequest : URLRequest = URLRequest(url: url)
-            urlRequest.httpBody = httpBody.data(using: .utf8)
-            urlRequest.httpMethod = httpMethod
-            
-            
-            var s:String?
-            session.dataTask(with: urlRequest) {
-                data, response, error in
-                if error != nil {
-                    print(error?.localizedDescription)
-                    completionHandler(error?.localizedDescription)
-                } else {
-                    s = String(data: data!, encoding: .utf8)!
-                    completionHandler(s)
-                }
-                }.resume()
-            
-        }catch{
-            print("errore di serializzazione")
-        }
-        
-        
-        
-        
-    }
+   
         
         
         
@@ -691,7 +602,8 @@ class SingletonServer{
         
         var dataFine =  date?.description
         for _ in 1...6 {
-           dataFine = String((dataFine?.dropLast())!)
+            
+            dataFine = String((dataFine?.dropLast())!)
         }
         print(dataFine)
         return dataFine!
